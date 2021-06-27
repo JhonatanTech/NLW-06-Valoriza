76959684b8256e0bb8ab072ecd942fbc2a6d5d30
Jhonatan Carvalho <jhonatancarvalhotorres12@gmail.com>
2021-06-27 16:37:47 -0300

    aula 5

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,44 @@
+import { getCustomRepository } from "typeorm";
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+
+import { UserRepositories } from "../repositories/UserRepositories";
+
+
+interface IAuthenticateRequest{
+    email: string;
+    password: string;
+}
+
+class AuthenticateUserService {
+    async execute({ email, password }: IAuthenticateRequest) {
+        const userRepositories = getCustomRepository(UserRepositories);
+
+        //verificar se email existe
+        const user = await userRepositories.findOne({
+            email
+        });
+
+        if (!user) {
+            throw new Error("Email/Password incorrect")
+        }
+
+        //varificar se senha etsa correta
+        const passwordMatch = await compare(password, user.password);
+        if (!passwordMatch) {
+            throw new Error("Email/Password incorrect")
+        }
+
+        //gerar token
+        const token = sign({
+            email: user.email
+        }, "8632edbe06e0f5c0809e6eadc7dd1247", {
+            subject: user.id,
+            expiresIn: "1d"
+        });
+
+        return token;
+    }
+}
+
+export { AuthenticateUserService };
